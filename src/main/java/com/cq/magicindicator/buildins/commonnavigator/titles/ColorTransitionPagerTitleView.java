71f4af90f3102250dbb2dc062679a20b875b3ffa
cq <cq@qq.com>
2020-09-05 17:30:03 +0800

    init

---
+++ src/main/java/com/cq/magicindicator/buildins/commonnavigator/titles/ColorTransitionPagerTitleView.java
@@ -0,0 +1,42 @@
+package com.cq.magicindicator.buildins.commonnavigator.titles;
+
+import android.content.Context;
+import android.graphics.Typeface;
+
+import com.cq.magicindicator.buildins.ArgbEvaluatorHolder;
+
+
+/**
+ * 两种颜色过渡的指示器标题
+ * 博客: http://hackware.lucode.net
+ * Created by hackware on 2016/6/26.
+ */
+public class ColorTransitionPagerTitleView extends SimplePagerTitleView {
+
+    public ColorTransitionPagerTitleView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onLeave(int index, int totalCount, float leavePercent, boolean leftToRight) {
+        int color = ArgbEvaluatorHolder.eval(leavePercent, mSelectedColor, mNormalColor);
+        setTextColor(color);
+    }
+
+    @Override
+    public void onEnter(int index, int totalCount, float enterPercent, boolean leftToRight) {
+        int color = ArgbEvaluatorHolder.eval(enterPercent, mNormalColor, mSelectedColor);
+        setTextColor(color);
+    }
+
+    @Override
+    public void onSelected(int index, int totalCount) {
+//        setFakeBoldText
+        setTypeface(Typeface.DEFAULT_BOLD);
+    }
+
+    @Override
+    public void onDeselected(int index, int totalCount) {
+        setTypeface(Typeface.DEFAULT);
+    }
+}
